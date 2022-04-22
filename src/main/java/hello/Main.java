f634a8e3b6c2c632b622129a17dd6595a09ae78d
leticiaprudente <let.prudente@gmail.com>
2022-04-21 22:43:30 -0300

    estrutura de pastas + hello world

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Hello World!!! (Main.java)");
+        }
+        catch(Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
